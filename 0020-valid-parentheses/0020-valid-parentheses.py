d8db336467e51d28cfd4d126b65edf53aa38978c
Fozia31 <foziyadamtew00@gmail.com>
2025-03-05 10:17:06 -0800

    Time: 3 ms (21.72%), Space: 17.8 MB (80.77%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,22 +1,29 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        stack=[]
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        open_brackets = [")" , "]" , "}"]
+        stack = []
         for i in s:
-            if i in ["]","}",")"]:
+            if i in open_brackets:
                 if stack:
-                    if (stack[-1]=="[" and i=="]") or (stack[-1]=="{" and i=="}") or(stack[-1]=="(" and i==")"):
+                    if stack[-1] == "("  and i == open_brackets[0] :
+                        stack.pop()
+                    elif stack[-1] == "["  and i == open_brackets[1] :
+                        stack.pop()
+                    elif stack[-1] == "{"  and i == open_brackets[2] :
                         stack.pop()
                     else:
-                        stack.append(i)
+                        return False
                 else:
-                    stack.append(i)
+                    return False
             else:
                 stack.append(i)
+                
         if stack:
             return False
-        return True
+        else:
+            return True
+
+                
+
         

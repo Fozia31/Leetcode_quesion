8858e7e87943025533cb42d590072e8f3bb59c8a
Fozia31 <foziyadamtew00@gmail.com>
2025-12-06 00:09:23 +0400

    Time: 0 ms (100%), Space: 12.5 MB (15.13%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 2:
+            return 1
+        if n == 3:
+            return 2
+
+        result = 1
+        while n > 4:
+            result *= 3
+            n -= 3
+        result *= n
+        return result

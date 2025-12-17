c3067deaa86cc86f51d12b24ae051304ae6d2f85
Foziya Damtew <foziyadamtew00@gmail.com>
2025-12-18 01:22:59 +0400

    Time: 3047 ms (7.32%), Space: 88.7 MB (7.32%) - LeetHub

---
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def maximumProfit(self, prices, k):
+        n = len(prices)
+        f = [[[0]*3 for _ in range(k+1)] for _ in range(n)]
+
+        for j in range(1, k+1):
+            f[0][j][1] = -prices[0]
+            f[0][j][2] = prices[0]
+
+        for i in range(1, n):
+            for j in range(1, k+1):
+                f[i][j][0] = max(
+                    f[i-1][j][0],
+                    f[i-1][j][1] + prices[i],
+                    f[i-1][j][2] - prices[i]
+                )
+                f[i][j][1] = max(
+                    f[i-1][j][1],
+                    f[i-1][j-1][0] - prices[i]
+                )
+                f[i][j][2] = max(
+                    f[i-1][j][2],
+                    f[i-1][j-1][0] + prices[i]
+                )
+
+        return f[n-1][k][0]

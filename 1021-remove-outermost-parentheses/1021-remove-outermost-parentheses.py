854552396db62f49136eb25fe078c946a4f00142
Foziya Damtew <foziyadamtew00@gmail.com>
2026-01-26 19:29:27 +0300

    Time: 4 ms (76.45%), Space: 12.7 MB (11.99%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeOuterParentheses(self, s):
+        res = []
+        depth = 0
+        for c in s:
+            if c == '(':
+                if depth > 0:
+                    res.append(c)
+                depth += 1
+            else:
+                depth -= 1
+                if depth > 0:
+                    res.append(c)
+        return "".join(res)

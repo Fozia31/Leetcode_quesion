91429575205865b0bf05f027efc7b18f82066228
Foziya Damtew <foziyadamtew00@gmail.com>
2026-02-15 19:11:35 +0300

    Time: 6 ms (43.39%), Space: 12.4 MB (89.89%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,4 +1,24 @@
-class Solution:
-    def addBinary(self, a: str, b: str) -> str:
-        sum_binary = bin(int(a, 2) + int(b, 2))
-        return sum_binary[2:]
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        result = []
+        carry = 0
+        i = len(a) - 1
+        j = len(b) - 1
+        
+        while i >= 0 or j >= 0 or carry:
+            digit_a = int(a[i]) if i >= 0 else 0
+            digit_b = int(b[j]) if j >= 0 else 0
+            
+            total = digit_a + digit_b + carry
+            carry = total // 2
+            result.append(str(total % 2))
+            
+            i -= 1
+            j -= 1
+        
+        return ''.join(result[::-1])
